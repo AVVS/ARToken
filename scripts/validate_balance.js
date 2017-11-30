b49ef7a409b653f1b30ca90f2c70b18d944d6e14
AVVS <v@makeomatic.ru>
2017-11-29 21:44:18 -0800

    chore: validate balance script

--- scripts/validate_balance.js
+++ scripts/validate_balance.js
@@ -2,6 +2,7 @@ const Promise = require('bluebird');
 const Papa = require('papaparse');
 const fs = require('fs');
 const path = require('path');
+const run = require(path.resolve(process.env.HOME, 'projects/firebase-tokensale/scripts/work'));
 
 const TokenAllocation = artifacts.require('./TokenAllocation.sol');
 const Cappasity = artifacts.require('./Cappasity.sol');
@@ -46,17 +47,26 @@ module.exports = async function() {
     return map;
   }, finalBalances);
 
-  // perform operations
-  await Promise.map(parsedData, async (payout) => {
-    const { user, saft, wallet: _wallet, usd, capp, bonus } = payout;
+  await run(async (database) => {
+    // perform operations
+    await Promise.map(parsedData, async (payout) => {
+      const { user, saft, wallet: _wallet, usd, capp, bonus } = payout;
 
-    const wallet = _wallet.indexOf('0x') === 0 ? _wallet : `0x${_wallet}`;
-    const destination = saft === 'yes' ? saftWallet : wallet;
-    const balance = await cappInstance.balanceOf(destination);
-    const expectedBalance = finalBalances[destination.toLowerCase()];
+      const wallet = _wallet.indexOf('0x') === 0 ? _wallet : `0x${_wallet}`;
+      const destination = saft === 'yes' ? saftWallet : wallet;
+      const balance = await cappInstance.balanceOf(destination);
+      const expectedBalance = finalBalances[destination.toLowerCase()];
 
-    if (balance.eq(expectedBalance) !== true) {
-      console.warn('abnormal balance - %j', payout);
-    }
-  }, { concurrency: 50 });
+      if (balance.eq(expectedBalance) !== true) {
+        const payoutRef = database.ref(`/payouts/${user}/phase_one`);
+        const txData = await payoutRef.once('value').then(s => s.val());
+        const balanceAtOldDestination = await cappInstance.balanceOf(txData.destination);
+        console.warn(
+          '[%s] - %s/%s/%s - %s - %s',
+          txData.state, balance.toNumber(), balanceAtOldDestination.toNumber(), capp,
+          destination, txData.destination
+        );
+      }
+    }, { concurrency: 50 });
+  });
 };
--- test/helpers/abi.js
+++ test/helpers/abi.js
@@ -2,10 +2,13 @@ const abi = require('ethereumjs-abi')
 
 const parameterTypes = ['address'];
 const parameterValues = ['0x711c5596d98081230bee9c8bc0c15d94eda16151'];
+const prodParameterValues = ['0xf67acb7b9226e482afcf7f08aac9466c50c19d9c'];
 
 const encoded = abi.rawEncode(parameterTypes, parameterValues);
+const prodEncoded = abi.rawEncode(parameterTypes, prodParameterValues);
 
 console.log(encoded.toString('hex'));
+console.log(prodEncoded.toString('hex'));
 
 const tokenTypes = ['address', 'address', 'address', 'address', 'address'];
 const tokenValues = [
@@ -15,10 +18,19 @@ const tokenValues = [
   '0x00bFeD8A3Ab20CAd28328A8ACfE7Db0700ede5c2',
   '0x004367dF01759d415b63430c6d6b242CEE2dF6de',
 ];
+const prodValues = [
+  '0x00dC895137042E1b0959E85E60747Ed9B58f0701',
+  '0x005c1E464F8d4422e08B0620C7ADcdcBbe0FB240',
+  '0x008ebEE8422f9Fe222a9B2C4A14A595846a457a4',
+  '0x00BBb48739cfd64B622776a22A57d741E48164C4',
+  '0x004899c9bAE1129fE359e6FAa2C97Ab6095C7335',
+];
 
 const token = abi.rawEncode(tokenTypes, tokenValues);
+const prodToken = abi.rawEncode(tokenTypes, prodValues);
 
 console.log(token.toString('hex'));
+console.log(prodToken.toString('hex'));
 
 const vestingTypes = ['address', 'address'];
 const vestingValues = [
